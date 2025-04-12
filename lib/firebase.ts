23ac00af64772f1aa16a585cdc72b78a49409592
iamommangukiya <ommangukiya10123@gmail.com>
2025-04-13 01:59:46 +0530

    FIREBASE ADDED

---
+++ lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCy1tsQSZfnG8FB1wfRbWOpBexEQu4chUg",
+    authDomain: "speakspace-9a3ee.firebaseapp.com",
+    databaseURL: "https://speakspace-9a3ee-default-rtdb.firebaseio.com",
+    projectId: "speakspace-9a3ee",
+    storageBucket: "speakspace-9a3ee.firebasestorage.app",
+    messagingSenderId: "221359219016",
+    appId: "1:221359219016:web:6d03627d703baf9ebd537d",
+    measurementId: "G-JP444HGR42"
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
